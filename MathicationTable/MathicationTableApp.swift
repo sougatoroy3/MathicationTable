a6cd3e2dcee86d865284b9c415f71272272b16c0
sougatoroy3 <sougatoroy007@gmail.com>
2025-08-04 18:22:04 +0530

    Initial Commit

---
+++ MathicationTable/MathicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathicationTableApp.swift
+//  MathicationTable
+//
+//  Created by Sougato Roy on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MathicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathicationTableTests/MathicationTableTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathicationTableTests.swift
+//  MathicationTableTests
+//
+//  Created by Sougato Roy on 04/08/25.
+//
+
+import Testing
+@testable import MathicationTable
+
+struct MathicationTableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
